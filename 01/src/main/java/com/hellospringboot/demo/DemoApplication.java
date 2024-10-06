7102741c8876eb3c2b9416475d45cb05eb9c2cf8
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2024-10-06 11:47:07 +0530

    added spring boot acutator and spring boot security

--- 01/src/main/java/com/hellospringboot/demo/DemoApplication.java
+++ 01/src/main/java/com/hellospringboot/demo/DemoApplication.java
@@ -14,8 +14,4 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
-	    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	      return String.format("Hello %s!\nHow are you?", name);
-    }
 }
