d151012c340c01127014fd2d1fe1ef2e3b984218
Vaibhav Bagate <bagatevaibhav555@gmai.com>
2024-12-13 13:22:34 +0530

    added application properties file and it's use in contoller

--- 00_intital_setup/src/main/java/com/hellospringboot/demo/RestRoutes/RestControllers.java
+++ 00_intital_setup/src/main/java/com/hellospringboot/demo/RestRoutes/RestControllers.java
@@ -1,13 +1,39 @@
 package com.hellospringboot.demo.RestRoutes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllers {
 
-@GetMapping("/")
-    public  String Home(){
-    return "Hello from spring bofefot!!!";
-}
+ //accessing the custom application properties
+   @Value("${company.name}")
+    private String companyName;
+
+    @Value("${company.ceo}")
+    private String companyCeo;
+
+    //new endpoint to return the propertied value
+    @GetMapping("/company")
+    public  String Company(){
+        return "Company:- "+companyName+" CEO:- "+companyCeo;
+    }
+
+    @GetMapping("/")
+    public String Home() {
+        return "Hello from spring home page!!!";
+    }
+
+    @GetMapping("/vb")
+    public String vb() {
+        return "Hello from Vaibhav!!";
+    }
+
+    @GetMapping("/sb")
+    public String sm() {
+        return "Hello from sumit!!";
+    }
+
+
 }
